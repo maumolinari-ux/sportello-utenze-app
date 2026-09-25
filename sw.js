29ca5608afb778e2682428cb3ca1a6ca6c55a2c2
maumolinari-ux <mau.molinari@gmail.com>
2026-09-25 18:56:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='su-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','logo.png','maurizio.jpg','milena.jpg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='su-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','logo.png','maurizio.jpg','milena.jpg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
